3035214e2414f3ffe14c4acbe73368401f104f30
aoliadi <aoliadi1998@gmail.com>
2022-08-12 21:02:20 +0100

    let's see

--- assets/js/learning.js
+++ assets/js/learning.js
@@ -9,26 +9,32 @@ const CHECKOUT_BTN = document.getElementById("btn--checkout-js");
 
 const AVAILABLE_GADGETS = [
   {
+    id: 1,
     name: "Samsung TV",
     price: 500000,
   },
   {
+    id: 2,
     name: "Pixel 4a",
     price: 250000,
   },
   {
+    id: 3,
     name: "PS 5",
     price: 300000,
   },
   {
+    id: 4,
     name: "MacBook Air",
     price: 800000,
   },
   {
+    id: 5,
     name: "Apple Watch",
     price: 95000,
   },
   {
+    id: 6,
     name: "Air Pods",
     price: 75000,
   },
@@ -40,20 +46,43 @@ CART_ICON.addEventListener("click", () =>
   MODAL_CONTAINER.classList.toggle("hidden")
 );
 
+function handleCounter(id, action) {
+  switch (action) {
+    case "decrement":
+      console.log("-1");
+      break;
+    case "increment":
+      console.log("+1");
+      break;
+    case "remove":
+      console.log("remove");
+      break;
+
+    default:
+      return;
+  }
+}
+
 function createInvoice(cartArr) {
   return cartArr
     .map((item, index) => {
       return `
     <tr>
-      <td></td>
+      <td>${index + 1}</td>
       <td>${item.name}</td>
       <td>${item.price}</td>
       <td>
-          <button class="btn--counter">-</button>
+          <button class="btn--counter" onclick="handleCounter(${
+            item.id
+          },'decrement')">-</button>
           <span class="option_counter_val">${item.quantity}</span>
-          <button class="btn--counter">+</button>
+          <button class="btn--counter" onclick="handleCounter(${
+            item.id
+          },'increment')">+</button>
       </td>
-      <td><button class="btn invoice_btn--remove">remove</button></td>
+      <td><button class="btn invoice_btn--remove" onclick="handleCounter(${
+        item.id
+      },'remove')">remove</button></td>
     </tr>
     `;
     })
@@ -94,7 +123,7 @@ function addToCart(gadget) {
 }
 
 function removeFromCart(gadget) {
-  CART_ARRAY = CART_ARRAY.filter((item) => item.name !== gadget.name);
+  CART_ARRAY = CART_ARRAY.filter((item) => item.id !== gadget.id);
 }
 
 function changeBtnLabel(btn, action) {
@@ -108,11 +137,11 @@ function changeBtnLabel(btn, action) {
 }
 
 function isAlreadySelected(gadget) {
-  return CART_ARRAY.find((elem) => elem.name === gadget.name);
+  return CART_ARRAY.find((elem) => elem.id === gadget.id);
 }
 
-function handleAddToCartProcess(event, id) {
-  const selectedGadget = AVAILABLE_GADGETS[id];
+function handleAddToCartProcess(event, index) {
+  const selectedGadget = AVAILABLE_GADGETS[index];
   const feedback = isAlreadySelected(selectedGadget);
 
   if (feedback) {
@@ -129,7 +158,7 @@ function handleAddToCartProcess(event, id) {
 let gadgetList = AVAILABLE_GADGETS.map((item, index) => {
   return `
   <div class="shop1">
-    <img src="./assets/img/product${index + 1}.png">
+    <img src="./assets/img/product${item.id}.png">
     <p>${item.name}</p>
     <h3 id="p3">${item.price}</h3>
     <div>
